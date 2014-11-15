47cbeac696413f7ff9dcc12f42dc1fd1ab6ad574
Ben Griffiths <bengriffiths@gmail.com>
2014-11-15 00:09:58 +0000

    Reformat

--- js/XYZWRenderer.js
+++ js/XYZWRenderer.js
@@ -1,8 +1,8 @@
 XYZWRenderer = function ( parameters ) {
-    this.parameters = parameters;
-    this.domElement = document.createElement("canvas");
-    this.width = this.domElement.width;
-    this.height = this.domElement.height;
+  this.parameters = parameters;
+  this.domElement = document.createElement("canvas");
+  this.width = this.domElement.width;
+  this.height = this.domElement.height;
 };
 
 XYZWRenderer.prototype = {
@@ -15,37 +15,37 @@ XYZWRenderer.prototype = {
   },
 
   projectVertex: function ( src, modelMatrix, viewProjectionMatrix ) {
-      var vertex = new THREE.RenderableVertex();
-      vertex.position.set(src.x, src.y, src.z);
+    var vertex = new THREE.RenderableVertex();
+    vertex.position.set(src.x, src.y, src.z);
 
-      var position = vertex.position;
-			var positionWorld = vertex.positionWorld;
-			var positionScreen = vertex.positionScreen;
+    var position = vertex.position;
+    var positionWorld = vertex.positionWorld;
+    var positionScreen = vertex.positionScreen;
 
-			positionWorld.copy( position ).applyMatrix4( modelMatrix );
-			positionScreen.copy( positionWorld ).applyMatrix4( viewProjectionMatrix );
+    positionWorld.copy( position ).applyMatrix4( modelMatrix );
+    positionScreen.copy( positionWorld ).applyMatrix4( viewProjectionMatrix );
 
-			var invW = 1 / positionScreen.w;
+    var invW = 1 / positionScreen.w;
 
-			positionScreen.x *= invW;
-			positionScreen.y *= invW;
-			positionScreen.z *= invW;
+    positionScreen.x *= invW;
+    positionScreen.y *= invW;
+    positionScreen.z *= invW;
 
-			vertex.visible = positionScreen.x >= - 1 && positionScreen.x <= 1 &&
-					 positionScreen.y >= - 1 && positionScreen.y <= 1 &&
-					 positionScreen.z >= - 1 && positionScreen.z <= 1;
+    vertex.visible = positionScreen.x >= - 1 && positionScreen.x <= 1 &&
+      positionScreen.y >= - 1 && positionScreen.y <= 1 &&
+      positionScreen.z >= - 1 && positionScreen.z <= 1;
 
-      return vertex;
-	},
+    return vertex;
+  },
 
   render: function(scene, camera) {
-	  var viewMatrix = new THREE.Matrix4(),
-	      viewProjectionMatrix = new THREE.Matrix4();
+    var viewMatrix = new THREE.Matrix4(),
+    viewProjectionMatrix = new THREE.Matrix4();
 
     scene.updateMatrixWorld();
 
-		viewMatrix.copy(camera.matrixWorldInverse);
-		viewProjectionMatrix.multiplyMatrices(camera.projectionMatrix, viewMatrix);
+    viewMatrix.copy(camera.matrixWorldInverse);
+    viewProjectionMatrix.multiplyMatrices(camera.projectionMatrix, viewMatrix);
 
     renderList = [];
 
@@ -55,8 +55,8 @@ XYZWRenderer.prototype = {
         var vertices = object.geometry.vertices;
         for (var v=0, vl=vertices.length; v < vl; v++) {
           var vertex = _this.projectVertex(vertices[v], 
-                                       object.matrixWorld, 
-                                       viewProjectionMatrix);
+            object.matrixWorld, 
+            viewProjectionMatrix);
           if (vertex.visible) {
             renderList.push(vertex);
           }
