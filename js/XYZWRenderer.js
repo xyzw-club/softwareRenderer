c0fcaa208951398aa8afa3856d7a2c9e40eac62b
Ben Griffiths <bengriffiths@gmail.com>
2014-11-25 23:58:08 +0000

    Per pixel rendering. Something not right about it.

--- js/XYZWRenderer.js
+++ js/XYZWRenderer.js
@@ -14,13 +14,14 @@ XYZWRenderer.prototype = {
     this.height = this.domElement.height;
   },
 
-  projectVertex: function ( src, modelMatrix, viewProjectionMatrix ) {
+  shadeVertex: function ( src, normal, modelMatrix, viewProjectionMatrix, lights ) {
     var vertex = new THREE.RenderableVertex();
     vertex.position.set(src.x, src.y, src.z);
 
     var position = vertex.position;
     var positionWorld = vertex.positionWorld;
     var positionScreen = vertex.positionScreen;
+
 
     positionWorld.copy( position ).applyMatrix4( modelMatrix );
     positionScreen.copy( positionWorld ).applyMatrix4( viewProjectionMatrix );
@@ -35,8 +36,24 @@ XYZWRenderer.prototype = {
       positionScreen.y >= - 1 && positionScreen.y <= 1 &&
       positionScreen.z >= - 1 && positionScreen.z <= 1;
 
+    // this only works for uniform scaling because of MATHS.
+    vertex.normalWorld = new THREE.Vector3().copy(normal).applyMatrix4(modelMatrix).normalize();
+
+    var ambient = new THREE.Color(0x020202);
+    var diffuse = new THREE.Color(0x827461);
+
+    for (var l=0, vl = lights.length; l < vl; l++) {
+      var light = lights[l];
+      var lightPosition = new THREE.Vector3().setFromMatrixPosition( light.matrixWorld ).normalize();
+      var lightWeight = Math.max(vertex.normalWorld.dot( lightPosition ), 0.0);
+      ambient.add( new THREE.Color(0xffffff).multiplyScalar(lightWeight)); 
+    }
+
+    vertex.colour = diffuse.multiply(ambient);
+
     return vertex;
   },
+
 
   render: function(scene, camera) {
     var viewMatrix = new THREE.Matrix4(),
@@ -48,25 +65,43 @@ XYZWRenderer.prototype = {
     viewProjectionMatrix.multiplyMatrices(camera.projectionMatrix, viewMatrix);
 
     renderList = [];
-
     var _this = this;
+
+    var rendered_vertex = function(idx, normal, shaded_vertices, object, lights) {
+      if (!(idx in shaded_vertices)) {
+        shaded_vertices[idx] = _this.shadeVertex(object.geometry.vertices[idx], normal, object.matrixWorld, viewProjectionMatrix, lights);
+      }
+      return shaded_vertices[idx];
+    };
+
+    var faceZ = function(face) {
+      return ( face[0].positionScreen.z + 
+          face[1].positionScreen.z + 
+          face[2].positionScreen.z ) / 3;
+    };
+
+    var lights = [];
+
+    scene.traverseVisible( function (object) {
+      if (object instanceof THREE.DirectionalLight) {
+        lights.push(object);
+      }
+    });
+
     scene.traverseVisible( function ( object ) {
       if (object instanceof THREE.Mesh) {
+        var shaded_vertices = {};
         var faces = object.geometry.faces;
-
 
         for (var v=0, vl=faces.length; v < vl; v++) {
           var face = faces[v];
-          var v1 = object.geometry.vertices[face.a];
-          var v2 = object.geometry.vertices[face.b];
-          var v3 = object.geometry.vertices[face.c];
 
-          v1 = _this.projectVertex(v1, object.matrixWorld, viewProjectionMatrix);
-          v2 = _this.projectVertex(v2, object.matrixWorld, viewProjectionMatrix);
-          v3 = _this.projectVertex(v3, object.matrixWorld, viewProjectionMatrix);
+          var a = rendered_vertex(face.a, face.vertexNormals[0], shaded_vertices, object, lights);
+          var b = rendered_vertex(face.b, face.vertexNormals[1], shaded_vertices, object, lights);
+          var c = rendered_vertex(face.c, face.vertexNormals[2], shaded_vertices, object, lights);
 
-          if (v1.visible || v2.visible || v3.visible) {
-            renderList.push([v1, v2, v3])
+          if (a.visible || b.visible || c.visible) {
+            renderList.push([a,b,c]);
           }
         }
       }
@@ -76,11 +111,15 @@ XYZWRenderer.prototype = {
     var context = canvas.getContext( '2d', {} );
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    renderList.sort( function(a, b) {
+      return faceZ(b) - faceZ(a);
+    });
+
     var to_canvas_position = function(vertex) {
-      var canvas_x = (vertex.positionScreen.x / 2.0 + 0.5) * canvas.width;
-      var canvas_y = (vertex.positionScreen.y / -2.0 + 0.5) * canvas.height;
-      return {x: canvas_x, y: canvas_y};
-    }
+      vertex.x = (vertex.positionScreen.x / 2.0 + 0.5) * canvas.width;
+      vertex.y = (vertex.positionScreen.y / -2.0 + 0.5) * canvas.height;
+      return vertex; 
+    };
 
     for (var v=0, vl=renderList.length; v < vl; v++) {
       var face = renderList[v];
@@ -88,16 +127,78 @@ XYZWRenderer.prototype = {
       var vv2 = to_canvas_position(face[1]);
       var vv3 = to_canvas_position(face[2]);
 
-      context.beginPath();
-
-      context.moveTo(vv1.x, vv1.y); 
-      context.lineTo(vv2.x, vv2.y); 
-      context.lineTo(vv3.x, vv3.y); 
-      context.lineTo(vv1.x, vv1.y); 
-
-      context.stroke();
+      _this.drawTriangle(context, vv1, vv2, vv3);
     }
 
   },
 
+  shadeFragment: function(colour) {
+    return colour;
+  },
+
+  drawTriangle: function(context, v1, v2, v3) {
+    // calculate bounding box
+    var maxX = Math.max(v1.x, Math.max(v2.x, v3.x));
+    var minX = Math.min(v1.x, Math.min(v2.x, v3.x));
+    var maxY = Math.max(v1.y, Math.max(v2.y, v3.y));
+    var minY = Math.min(v1.y, Math.min(v2.y, v3.y));
+
+    var base = new THREE.Vector2(v1.x, v1.y);
+    var vv0 = new THREE.Vector2(v2.x, v2.y).sub(base);
+    var vv1 = new THREE.Vector2(v3.x, v3.y).sub(base);
+
+    face = new RenderableFace(v1, v2, v3, context);
+
+    for (var x = minX; x <= maxX; x++)
+    {
+      for (var y = minY; y <= maxY; y++)
+      {
+        var vv2 = new THREE.Vector2(x,y).sub(base);
+
+        var dot00 = vv0.dot(vv0);
+        var dot01 = vv0.dot(vv1);
+        var dot02 = vv0.dot(vv2);
+        var dot11 = vv1.dot(vv1);
+        var dot12 = vv1.dot(vv2);
+
+        var invDenom = 1  / (dot00 * dot11 - dot01 * dot01);
+
+
+        var s = (dot11 * dot02 - dot01 * dot12) * invDenom; 
+        var t = (dot00 * dot12 - dot01 * dot02) * invDenom; 
+
+        if ( (s >= 0) && (t >= 0) && (s + t <= 1))
+        { 
+          face.render(x,y,s,t,this.shadeFragment);  
+        }
+      }
+    }
+  }
+
+
 };
+
+RenderableFace = function(v1, v2, v3, context) {
+  this.v1 = v1;
+  this.v2 = v2;
+  this.v3 = v3;
+  this.context = context;
+};
+
+
+RenderableFace.prototype = {
+  render: function(x,y,s,t,shader) {
+    var w = 1 - (s+t);
+
+    var c1s = new THREE.Color().copy(this.v1.colour).multiplyScalar(t);
+    var c2s = new THREE.Color().copy(this.v2.colour).multiplyScalar(w);
+    var c3s = new THREE.Color().copy(this.v3.colour).multiplyScalar(s);
+
+    c1s.add(c2s).add(c3s);    
+    var color = shader(c1s);
+    color.a = 1.0;
+    this.context.fillStyle = color.getStyle();
+    this.context.fillRect(x,y,1,1);
+  }
+};
+
